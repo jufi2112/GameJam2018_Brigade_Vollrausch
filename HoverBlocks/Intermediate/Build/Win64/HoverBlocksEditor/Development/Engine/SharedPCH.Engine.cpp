90428df65fe421db8bb295f82d4368502044e7f4
Julien Fischer <jufi2112@gmail.com>
2018-11-02 19:36:59 +0100

    initial commit

---
+++ HoverBlocks/Intermediate/Build/Win64/HoverBlocksEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/Users/Julien/Documents/GameJam/HoverBlocks/Intermediate/Build/Win64/HoverBlocksEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ HoverBlocks/Source/HoverBlocks/HoverBlocksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoverBlocksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOVERBLOCKS_API AHoverBlocksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
